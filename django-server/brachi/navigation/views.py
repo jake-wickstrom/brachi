6263701cff797c81141ce3650eb4bda48dded33d
Sonny Cervienka <sonnycervienka@gmail.com>
2018-03-14 11:52:03 -0700

    simple level select page

--- django-server/brachi/navigation/views.py
+++ django-server/brachi/navigation/views.py
@@ -17,12 +17,15 @@ def login(request):
                 # TODO: create session
                 return level_select(request)
         except Exception as e:
+            print(e)
             pass # must be an invalid input, just let the return below catch it
     # if anything about fails, come here
     return title(request, invalid_name=True)
 
 def level_select(request):
     context = {}
+    # TODO: update this, maybe find a better place to put a global constant
+    context['levels'] = ['Brachistochrone', 'Brachistochrone^2', 'Level 3']
     return render(request, 'navigation/level_select.html', context)
 
 def leaderboard(request):
@@ -30,6 +33,9 @@ def leaderboard(request):
     return render(request, 'navigation/leaderboard.html', context)
 
 def play(request):
+    # TODO: process GET request and return appropriate level
+    print(request.GET['level'])
+    return level_select(request)
     pass
 
 def project(request):
